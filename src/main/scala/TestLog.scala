4dd56ea88468170b62e7d1a2276b7ba086f93775
PhamDuyHieutb <phamduyhieuit@gmail.com>
2017-09-25 19:34:33 +0700

    second

--- src/main/scala/TestLog.scala
+++ src/main/scala/TestLog.scala
@@ -20,7 +20,7 @@ object TestLog{
         }
 
     }
-  
+
 
 
 
@@ -32,23 +32,38 @@ object TestLog{
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
 //    val spark = SparkSession.builder().master("local").appName("Log Query").enableHiveSupport().getOrCreate()
-    val logdata = sqlContext.read.parquet("/data/Parquet/AdnLog/2017_09_19/{parquet_logfile_at_21h_00.snap,parquet_logfile_at_21h_05.snap,parquet_logfile_at_21h_10.snap}")
+    val logdata = sqlContext.read.parquet("/data/Parquet/AdnLog/2017_09_19/{parquet_logfile_at_21h_00.snap,parquet_logfile_at_21h_05.snap,parquet_logfile_at_21h_10.snap}").repartition(10)
 
 
 
     logdata.registerTempTable("log")
 //    val stringify = udf((vs: Seq[BigInt]) => vs.mkString(",") )
-    val sqlTrue = sqlContext.sql("select guid,bannerId,time_group.time_create from log where click_or_view = true").rdd
-    val sqlFalse= sqlContext.sql("select guid,bannerId,time_group.time_create from log ").rdd
+
+    val sql= sqlContext.sql("select guid,bannerId,time_group.time_create,click_or_view from log ").rdd
 //    sql.write.format("com.databricks.spark.csv").csv("/home/hadoop/result.csv")
 //    val stringify = udf( (time_create:BigInt,cookie_create:BigInt) => castToString(time_create,cookie_create) )
-    val sqlClickResult = sqlTrue.map(e => ((e.getLong(0), e.getInt(1)), ("c",e.getLong(2))))
-    val sqlImpressionResult = sqlFalse.map(e => ((e.getLong(0), e.getInt(1)),("i",e.getLong(2))))
+    val sqlClickResult = sql.map(e => {
+    var v="0_1"
+    if (e.getBoolean(3)) v ="1_1"
+  ((e.getLong(0), e.getInt(1),(e.getLong(2)/900000)*900000), (v))
+})
+//    val sqlImpressionResult = sql.map(e => ((e.getLong(0), e.getInt(1),(e.getLong(2)/900000)*900000),(0)))
+
+
+    val re = sqlClickResult.reduceByKey((v1,v2) => {
+      var a1 = v1.split("_")
+      var a2 = v2.split("_")
+      return a1(0).toInt+a2(0).toInt +"_"+a1(1).toInt+a2(1).toInt
+    }).map((a) =>{
+      var p = a._2.split("_")
+      var result = (p(0).toInt)*1.0/(p(0).toInt+p(1).toInt)
+      return (a._1,result)
+    } ).saveAsTextFile("/user/hieupd/logAnalysist/part4")
 
 //    val re = sqlImpressionResult.union(sqlClickResult).reduceByKey(Combine).coalesce(1).saveAsTextFile("/user/hieupd/logAnalysist/part1")
-    val reClick = sqlImpressionResult.union(sqlClickResult).reduceByKey(Combine).mapValues{a: (String,Long)=> a._1}.map(a => (a._2,1)).reduceByKey((a1,a2)=> a1+a2)
-    val dem = reClick.map(a => a._2).sum()
-    val ctr = reClick.map(a => (a._1,a._2/dem)).saveAsTextFile("/user/hieupd/logAnalysist/part2")
+//    val reClick = sqlImpressionResult.union(sqlClickResult).reduceByKey(Combine).mapValues{a: (String,Long)=> a._1}.map(a => (a._2,1)).reduceByKey((a1,a2)=> a1+a2)
+//    val dem = reClick.map(a => a._2).sum()
+//    val ctr = reClick.map(a => (a._1,a._2/dem)).repartition(1).saveAsTextFile("/user/hieupd/logAnalysist/part2")
 
   }
 }
